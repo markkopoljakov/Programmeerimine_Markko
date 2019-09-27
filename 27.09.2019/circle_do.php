0995d1269521254f5d66221322910ee2ea01e699
Markko Poljakov <markkopoljakov@Markkos-MacBook-Pro.local>
2019-09-27 15:36:49 +0300

    Ringi pindala ja ümbermõõdu arvutamine

--- 27.09.2019/circle_do.php
+++ 27.09.2019/circle_do.php
@@ -4,8 +4,13 @@ $circleRadius = $_GET['circle-radius'];
 // $sum = $circleRadius +  $circleRadius;
 // var_dump($sum);
 // arvutused
-$circleArea = 3.14 * $circleRadius * $circleRadius;
-$circleCircumference = 3.14 * 2 * $circleRadius;
+$circleArea = round(pi() * $circleRadius * $circleRadius, 3);
+$circleArea = pi() * 2 * $circleRadius * $circleRadius;
+$circleCircumference = pi() * 2 * $circleRadius;
+// $circleAreaRound = round($circleArea, 3);
+$circleArea = round($circleArea, 3);
+
+
 echo 'Raadius =' . $circleRadius . '<br>';
 echo '<hr>';
 echo 'Ringi pindala = ' . $circleArea . ' <br>';
